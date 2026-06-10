7fb917be472cd52af3137c0adeece18a2bb65be9
4uLmost <muhammaddhiyaul273@gmail.com>
2026-06-10 22:12:33 +0700

    menambahkan Runtie Configuration & StatusPesananMachine(automata)

---
+++ Reiat.Lib/Reiat.Lib/KonfigurasiAplikasi.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reiat.Lib.Reiat.Lib
+{
+    // Teknik Runtime Configuration
+    public class KonfigurasiAplikasi
+    {
+        public decimal Ppn { get; private set; }
+
+        public KonfigurasiAplikasi()
+        {
+            // Simulasi membaca dari file config saat runtime
+            Ppn = 0.11m; // 11%
+        }
+
+        public decimal HitungPpn(decimal nominal)
+        {
+            if (nominal < 0) throw new ArgumentException("Nominal tidak boleh negatif."); // DbC
+            return nominal * Ppn;
+        }
+    }
+}
---
+++ Reiat.Lib/StatusPesananMachine.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reiat.Lib
+{
+    // Automata
+    public enum OrderState { Keranjang, Checkout, Dibayar, Selesai }
+
+    public class StatusPesananMachine
+    {
+        public OrderState StateSaatIni { get; private set; } = OrderState.Keranjang;
+
+        public void LanjutKeCheckout()
+        {
+            if (StateSaatIni != OrderState.Keranjang)
+                throw new InvalidOperationException("Pesanan belum siap di-checkout."); // DbC
+            StateSaatIni = OrderState.Checkout;
+        }
+
+        public void ProsesPembayaran()
+        {
+            if (StateSaatIni != OrderState.Checkout)
+                throw new InvalidOperationException("Pesanan harus di-checkout sebelum dibayar."); // DbC
+            StateSaatIni = OrderState.Dibayar;
+        }
+
+        public void SelesaikanPesanan()
+        {
+            if (StateSaatIni != OrderState.Dibayar)
+                throw new InvalidOperationException("Pesanan belum dibayar."); // DbC
+            StateSaatIni = OrderState.Selesai;
+        }
+    }
+}
